0adb8e284e81f6af3c9f42b44bef046485db25e1
John Maguire <john.maguire@gmail.com>
2017-04-01 20:00:52 +0100

    Set pin 18 high on request to open door

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
-import "fmt"
+import "io"
+import "log"
+import "net/http"
+import "time"
+
 import "github.com/stianeikeland/go-rpio"
 
-func main() {
-	fmt.Println("foo")
+type gpioHandler struct {
+  pin rpio.Pin
+}
 
-	err := rpio.Open()
-	defer rpio.Close()
+func GpioHandler(pin rpio.Pin) http.Handler {
+  return &gpioHandler{pin}
+}
+
+func (f *gpioHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  io.WriteString(w, "hello, gpio!\n")
+
+  timer := time.NewTimer(time.Second * 5)
+  go func() {
+    f.pin.Output()
+    f.pin.High()
+    <-timer.C
+    f.pin.Low()
+  }()
+}
+
+func main() {
+  err := rpio.Open()
+  defer rpio.Close()
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  http.Handle("/hodoor", GpioHandler(rpio.Pin(18)))
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
